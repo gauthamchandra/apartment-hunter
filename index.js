83bda6a237fc0995d204ee402910c0f2a34adb34
Gautham Chandra <gautham.chandra@live.com>
2016-12-27 03:25:21 -0500

    Add provider to get transit times
    Not hooked up to the rest of the app
    but the the functionality is there

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var CraigslistQueryBuilder = require('./providers/craigslist_query_builder.js')
-  , CraigslistProvider = require('./providers/craigslist_provider.js');
+var env = require('./env.json')
+  , Promise = global.Promise
+  , CraigslistQueryBuilder = require('./providers/craigslist_query_builder.js')
+  , CraigslistProvider = require('./providers/craigslist_provider.js')
+  , TransitInfoProvider = require('./providers/transit_info_provider.js');
 
 var query = new CraigslistQueryBuilder()
   .setLocation('newyork')
@@ -14,8 +17,21 @@ var query = new CraigslistQueryBuilder()
 
 console.log('Fetching data using query:', query); 
 
-new CraigslistProvider(query)
-  .fetchFeed()
-  .then(posts => {
-    console.log(posts);
-  });
+//new CraigslistProvider(query)
+//  .fetchFeed()
+//  .then(posts => {
+//    console.log(posts[0]);
+//  });
+//
+
+var transitInfoProvider = new TransitInfoProvider(env.GOOGLE_MAPS_API_KEY);
+
+Promise.all([
+  transitInfoProvider.getLatLng('1600 Pennsylvania Ave NW, Washington, DC 20500'),
+  transitInfoProvider.getLatLng('United Nations, New York, NY')
+]).then(locations => {
+  transitInfoProvider.getTransitTime(locations[0], locations[1])
+    .then(value => {
+      console.log('Value:', value);
+    });
+});
---
+++ providers/transit_info_provider.js
@@ -0,0 +1,51 @@
+var Promise = global.Promise;
+
+class TransitInfoProvider {
+  constructor(mapsApiKey) {
+    this._googleMapsClient = require('@google/maps').createClient({
+      key: mapsApiKey,
+      Promise: Promise
+    });
+  }
+
+  getTransitTime(originLatLng, destLatLng) {
+    return new Promise((resolve, reject) => {
+      this._googleMapsClient.distanceMatrix({
+        mode: 'transit',
+        origins: [originLatLng],
+        destinations: [destLatLng]
+      }).asPromise().then(resp => {
+        if (resp.status === 200) {
+          resolve(resp.json.rows[0].elements[0].duration.value);
+        }
+        else {
+          reject(resp.status);
+        }
+      }).catch(error => {
+        console.error('Error when trying to determine transit time', error);
+      });
+    });
+  }
+
+  getLatLng(address) {
+    return new Promise((resolve, reject) => {
+      this._googleMapsClient.geocode({
+        address: address
+      }).asPromise().then(response => {
+        let firstResult = response.json.results[0];
+
+        if (firstResult) {
+          resolve(firstResult.geometry.location);
+        }
+        else {
+          resolve(null);
+        }
+      }).catch(error => {
+        console.error('An error occurred with geocode request:', error);
+        reject(error);
+      });
+    });
+  }
+}
+
+module.exports = TransitInfoProvider;
