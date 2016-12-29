852eaf5a4235180dc10c80db46aec4923860aee3
Gautham Chandra <gautham.chandra@live.com>
2016-12-28 20:06:06 -0500

    Support reading query params from file
    This will separate any PII from showing up in
    the actual source files

--- providers/craigslist/craigslist_query_builder.js
+++ providers/craigslist/craigslist_query_builder.js
@@ -1,9 +1,53 @@
-var querystring = require('querystring')
+var path = require('path')
+  , querystring = require('querystring')
   , CraigslistQuery = require('./craigslist_query.js');
 
 class CraigslistQueryBuilder {
   constructor() {
     this.options = { format: 'rss' };
+  }
+
+  /**
+   * Convenience method to move the query configuration to a
+   * json file
+   * */
+  readFromFile(pathRelativeToProjectRoot) {
+    var projectRoot = path.dirname(require.main.filename);
+    var json = require(`${projectRoot}/${pathRelativeToProjectRoot}`);
+
+    if (typeof json.region === 'string') {
+      this.setLocation(json.region);
+    }
+    if (typeof json.min_price === 'number') {
+      this.setMinimumPrice(json.min_price);
+    }
+    if (typeof json.max_price === 'number') {
+      this.setMaximumPrice(json.max_price);
+    }
+    if (typeof json.min_bedroms === 'number') {
+      this.setMinimumBedrooms(json.min_bedrooms);
+    }
+    if (typeof json.min_bathrooms === 'number') {
+      this.setMinimumBathrooms(json.min_bathrooms);
+    }
+    if (typeof json.search_postal_code === 'number' &&
+        typeof json.search_radius === 'number') {
+      this.within(json.search_postal_code, json.search_radius);
+    }
+    if (!!json.apartments_only) {
+      this.filterToApartments();
+    }
+    if (!!json.bundle_duplicates) {
+      this.bundleDuplicates();
+    }
+    if (!!json.posts_with_images_only) {
+      this.filterToPostsWithImages();
+    }
+    if (json.exclude instanceof Array) {
+      this.setExclusionFilters(json.exclude);
+    }
+
+    return this;
   }
 
   setLocation(loc) {
